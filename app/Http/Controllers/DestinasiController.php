795c653dd82ed52d4f6b519e49d24d754df80f3d
abi <abi.rahmawan01@students.uns.ac.id>
2022-12-01 22:58:21 +0700

    Update DestinasiController

--- app/Http/Controllers/DestinasiController.php
+++ app/Http/Controllers/DestinasiController.php
@@ -1,266 +1,170 @@
 <?php
 
-namespace App\Http\Controllers;
+declare(strict_types=1);
 
-use Illuminate\Http\Request;
+namespace App\Http\Controllers\DestinasiController;
+
 use App\Http\Controllers\Controller;
-use App\Repositories\Destinasi\DestinasiRepository;
 use App\Models\Destinasi;
 use App\Transformers\DestinasiTransformer;
-use Illuminate\Support\Facades\Log;
+use Domain\User\Actions\CreateUserAction;
+use Domain\User\Actions\FindUserByRouteKeyAction;
+use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class DestinasiController extends Controller
 {
-    protected array $permission = Destinasi::PERMISSIONS;
-    protected DestinasiRepository $destinasiRepository;
-
-    /** 
-     * @OA\Schema(
-     *      schema="destinasi__request_property",
-     *      @OA\Property(property="nama", type="string", example="nama 1"),
-     *      @OA\Property(property="alamat", type="string", example="alamat 1"),
-     *      @OA\Property(property="deskripsi", type="string", example="deskripsi 1"),
-     *      @OA\Property(property="kota_id", type="string", example="1")
-     * )
-     * 
-     * @OA\Schema(
-     *      schema="destinasi__response_property",
-     *      @OA\Property(property="data",type="array",
-     *          @OA\Items(
-     *              @OA\Property(property="type", type="string", example="destinasi"),
-     *              @OA\Property(property="id", type="string", example="1"),
-     *              @OA\Property(
-     *                  property="attributes", type="object",
-     *                  @OA\Property(property="nama", type="string", example="nama 1"),
-     *                  @OA\Property(property="alamat", type="string", example="alamat 1"),
-     *                  @OA\Property(property="deskripsi", type="string", example="deskripsi 1"),
-     *                  @OA\Property(property="kota_id", type="string", example="1")
-     *              ),  
-     *          )
-     *      )
-     * )
-     * 
-     * issue: https://github.com/zircote/swagger-php/issues/695 (swagger doesn't accep square bracket)
-     */
-    public function __construct(DestinasiRepository $destinasiRepository) 
+    public function __construct()
     {
-        $this->destinasiRepository = $destinasiRepository;
+        $permissions = User::PERMISSIONS;
 
-        $this->middleware(sprintf('permission:%s', $this->permission['create']), ['only' => ['create']]);
-        $this->middleware(sprintf('permission:%s', $this->permission['update']), ['only' => ['update']]);
+        $this->middleware('permission:'.$permissions['index'], ['only' => 'index']);
+        $this->middleware('permission:'.$permissions['create'], ['only' => 'store']);
+        $this->middleware('permission:'.$permissions['show'], ['only' => 'show']);
+        $this->middleware('permission:'.$permissions['update'], ['only' => 'update']);
+        $this->middleware('permission:'.$permissions['destroy'], ['only' => 'destroy']);
     }
 
     /**
-     * @api                {post} /destinasi
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Spatie\Fractal\Fractal
+     * @api                {get} /auth/users Get all users
+     * @apiName            get-all-users
+     * @apiGroup           User
+     * @apiVersion         1.0.0
      * @apiPermission      Authenticated User
-     * 
-     * @OA\Post(
-     *     path="/destinasi",
-     *     summary="Create deskripsi",
-     *     tags={"Deskripsi"},
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(ref="#/components/schemas/destinasi__request_property",)
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="200",
-     *         description="ok",
-     *         content={
-     *             @OA\MediaType(
-     *                 mediaType="application/json",
-     *                 @OA\Schema(ref="#/components/schemas/destinasi__response_property")
-     *             )
-     *         }
-     *     ),
-     *     security={{"authorization":{}}}
-     * )
+     * @apiUse             UsersResponse
+     *
      */
-    public function create(Request $request)
+    public function index()
+    {
+        return $this->fractal(
+            QueryBuilder::for(User::class)
+                ->allowedFilters(['nama', 'alamat', 'deskripsi', 'kota_id'])
+                ->paginate(),
+            new DestinasiTransformer()
+        );
+    }
+
+    /**
+     * @param  string  $id
+     *
+     * @return \Spatie\Fractal\Fractal
+     * @api                {get} /auth/users/{id} Show user
+     * @apiName            show-user
+     * @apiGroup           User
+     * @apiVersion         1.0.0
+     * @apiPermission      Authenticated User
+     * @apiUse             UserResponse
+     *
+     */
+    public function show(string $id)
+    {
+        return $this->fractal(
+            app(FindUserByRouteKeyAction::class)->execute($id, throw404: true),
+            new DestinasiTransformer()
+        );
+    }
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     * @api                {post} /auth/users Store user
+     * @apiName            store-user
+     * @apiGroup           User
+     * @apiVersion         1.0.0
+     * @apiPermission      Authenticated User
+     * @apiUse             UserCreatedResponse
+     * @apiParam {String} nama (required)
+     * @apiParam {String} alamat (required)
+     * @apiParam {String} deskripsi (required)
+     * @apiParam {integer} kota_id (required)
+     *
+     */
+    public function store(Request $request)
     {
         $attributes = $this->validate(
             $request,
             [
-                'nama'               => 'required|string',
-                'alamat'             => 'required|string',
-                'deskripsi'          => 'required|string',
-                'kota_id'            => 'required|string',
+                'nama'      => 'required|string',
+                'alamat'    => 'required|string',
+                'deskripsi' => 'required|string',
+                'kota_id'   => 'required|integer',
             ]
         );
 
-        $deskripsi = $this->destinasiRepository->create($attributes);
-        // $deskripsi = Destinasi::all();
-        return $this->fractal([$deskripsi], new DestinasiTransformer());
+        return $this->fractal(
+            app(CreateUserAction::class)->execute($attributes),
+            new DestinasiTransformer()
+        )
+            ->respond(201);
     }
 
     /**
-     * @OA\Get(
-     *     path="/destinasi",
-     *     summary="Get deskripsi",
-     *     tags={"Deskripsi"},
-     *     @OA\Parameter(name="page", in="query", required=false,),
-     *     @OA\Parameter(name="per_page", in="query", required=false,),
-     *     @OA\Parameter(name="nama", in="query", required=false,),
-     *     @OA\Parameter(name="alamat", in="query", required=false,),
-     *     @OA\Parameter(name="deskripsi", in="query", required=false,),
-     *     @OA\Parameter(name="kota_id", in="query", required=false,),
-     *     @OA\Response(
-     *         response="200",
-     *         description="ok",
-     *         content={
-     *             @OA\MediaType(
-     *                 mediaType="application/json",
-     *                 @OA\Schema(ref="#/components/schemas/destinasi__response_property")
-     *             )
-     *         }
-     *     ),
-     * )
-     */
-    public function find(Request $request)
-    {
-        $this->validate($request, [
-            'page'                  => 'nullable|integer',
-            'per_page'              => 'nullable|integer',
-            'nama'                  => 'nullable|string',
-            'alamat'                => 'nullable|string',
-            'deskripsi'             => 'nullable|string',
-            'kota_id'               => 'nullable|string',
-        ]);
-
-        $input = [
-            'per_page'              => $request->input('per_page', false),
-            'nama'                   => $request->input('nama', false),
-            'alamat'                 => $request->input('alamat', false),
-            'deskripsi'           => $request->input('deskripsi', false),
-            'kota_id'           => $request->input('kota_id', false),
-        ];
-        $deskripsis = $this->destinasiRepository->findByParams($input);
-        return $this->fractal($destinasis, new DestinasiTransformer());
-    }
-
-    /**
-     * @api                {get} /destinasi/{id}
-     * 
-     * @OA\Get(
-     *     path="/destinasi/{id}",
-     *     summary="Get destinasi By Id",
-     *     tags={"Destinasi"},
-     *     @OA\Parameter(name="id", in="path", required=true,),
-     *     @OA\Response(
-     *         response="200",
-     *         description="ok",
-     *         content={
-     *             @OA\MediaType(
-     *                 mediaType="application/json",
-     *                 @OA\Schema(ref="#/components/schemas/destinasi__response_property")
-     *             )
-     *         }
-     *     ),
-     *     security={{"authorization":{}}}
-     * )
-     */
-    public function findById(string $id)
-    {
-        if (!$id) {
-            abort(404);
-        }
-
-        $destinasi = $this->destinasiRepository->findByRouteKeyName($id);
-
-        if (blank($destinasi)) {
-            return $this->getErrorMessage(105);
-        }
-
-        return $this->fractal([$destinasi], new DestinasiTransformer());
-    }
-
-    /**
-     * @api                {put} /destinasi
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $id
+     *
+     * @return \Spatie\Fractal\Fractal
+     * @throws \Illuminate\Validation\ValidationException
+     * @api                {put} /auth/users/ Update user
+     * @apiName            update-user
+     * @apiGroup           User
+     * @apiVersion         1.0.0
      * @apiPermission      Authenticated User
-     * 
-     * @OA\Put(
-     *     path="/destinasi/{id}",
-     *     summary="Update destinasi",
-     *     tags={"Destinasi"},
-     *     @OA\Parameter(name="id", in="path", required=true,),
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(ref="#/components/schemas/destinasi__request_property",)
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="200",
-     *         description="ok",
-     *         content={
-     *             @OA\MediaType(
-     *                 mediaType="application/json",
-     *                 @OA\Schema(ref="#/components/schemas/destinasi__response_property")
-     *             )
-     *         }
-     *     ),
-     *     security={{"authorization":{}}}
-     * )
+     * @apiUse             UserResponse
+     * @apiParam {String} nama
+     * @apiParam {String} alamat
+     * @apiParam {String} deskripsi
+     * @apiParam {integer} kota_id
+     *
      */
     public function update(Request $request, string $id)
     {
         $attributes = $this->validate(
             $request,
             [
-                'nama'               => 'required|string',
-                'alamat'             => 'required|string',
-                'deskripsi'       => 'required|string',
-                'kota_id'       => 'required|string',
+                'nama'      => 'required|string',
+                'alamat'    => 'required|string',
+                'deskripsi' => 'required|string',
+                'kota_id'   => 'required|integer',
             ]
         );
 
-        $keyId = $this->destinasiRepository->findByRouteKeyName($id);
-        if (empty($keyId)) {
-            return response(['message' => 'Not found id.'], 403);
-        }
-        $id = $keyId->getKey();
-        $destinasi = $this->destinasiRepository->update($attributes, $id);
-        return $this->fractal([$destinasi], new DestinasiTransformer());
+        $destinasi = app(FindUserByRouteKeyAction::class)
+            ->execute($id);
+
+        $destinasi->update($attributes);
+
+        return $this->fractal($destinasi->refresh(), new DestinasiTransformer());
     }
 
-
     /**
+     * @param  string  $id
+     *
+     * @return \Illuminate\Http\Response|\Laravel\Lumen\Http\ResponseFactory
      * @api                {delete} /auth/users/{id} Destroy user
+     * @apiName            destroy-user
+     * @apiGroup           User
+     * @apiVersion         1.0.0
      * @apiPermission      Authenticated User
-     * @OA\Delete(
-     *     path="/destinasi/{id}",
-     *     summary="Delete destinasi",
-     *     tags={"Destinasi"},
-     *     @OA\Parameter(name="id", in="path", required=true,),
-     *     @OA\RequestBody(
-     *         @OA\MediaType(
-     *             mediaType="application/json",
-     *             @OA\Schema(ref="#/components/schemas/destinasi__request_property",)
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response="200",
-     *         description="ok"
-     *         content={
-     *             @OA\MediaType(
-     *                 mediaType="application/json",
-     *                 @OA\Schema(ref="#/components/schemas/destinasi__response_property")
-     *             )
-     *         }
-     *     ),
-     *     security={{"authorization":{}}}
-     * )
+     * @apiUse             NoContentResponse
+     *
      */
-
     public function destroy(string $id)
     {
-        $id = $this->destinasiRepository->findByRouteKeyName($id)->getKey();
-        if (empty($id)) {
-            return $this->getErrorMessage(105);
+        $user = app(FindUserByRouteKeyAction::class)
+            ->execute($id);
+
+        if (app('auth')->id() == $user->getKey()) {
+            return response(['message' => 'You cannot delete your self.'], 403);
         }
 
-        $this->destinasiRepository->delete($id);
+        $user->delete();
+
         return response('', 204);
     }
 }
