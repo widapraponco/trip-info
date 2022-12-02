bce00e30d715be1ef15bec2b9e4bff430e1b8808
abi <abi.rahmawan01@students.uns.ac.id>
2022-12-02 14:26:49 +0700

    Update Routes Destinasi

--- routes/Destinasi.php
+++ routes/Destinasi.php
@@ -1,14 +1,79 @@
 <?php
 
+declare(strict_types=1);
+
+/** @var Laravel\Lumen\Routing\Router $router */
+
 $router->group(
     [
-        'as' => 'destinasi',
+        'as' => 'destinasis',
     ],
     function () use ($router) {
-        $router->post('/destinasi', ['middleware' => ['auth'], 'uses'=> 'DestinasiController@create',]);
-        $router->get('/destinasi', ['uses' => 'DestinasiController@find',]);
-        $router->get('/destinasi/{id}', ['middleware' => ['auth'], 'uses' => 'DestinasiController@findById',]);
-        $router->put('/destinasi/{id}', ['middleware' => ['auth'], 'uses'=> 'DestinasiController@update',]);
-        $router->delete('/destinasi/{id}', ['middleware' => ['auth'], 'uses'=> 'DestinasiController@destroy',]);
+        $router->group(
+            [
+                'prefix' => 'destinasis',
+            ],
+            function () use ($router) {
+                // deletes
+                $router->get(
+                    '/deleted',
+                    [
+                        'as' => 'deleted',
+                        'uses' => 'DestinasiDeleteController@deleted',
+                    ]
+                );
+                $router->put(
+                    '/{id}/restore',
+                    [
+                        'as' => 'restore',
+                        'uses' => 'DestinasiDeleteController@restore',
+                    ]
+                );
+                $router->delete(
+                    '/{id}/purge',
+                    [
+                        'as' => 'purge',
+                        'uses' => 'DestinasiDeleteController@purge',
+                    ]
+                );
+
+                // resources
+                $router->get(
+                    '/',
+                    [
+                        'as' => 'index',
+                        'uses' => 'DestinasiController@index',
+                    ]
+                );
+                $router->post(
+                    '/',
+                    [
+                        'as' => 'store',
+                        'uses' => 'DestinasiController@store',
+                    ]
+                );
+                $router->get(
+                    '/{id}',
+                    [
+                        'as' => 'show',
+                        'uses' => 'DestinasiController@show',
+                    ]
+                );
+                $router->put(
+                    '/{id}',
+                    [
+                        'as' => 'update',
+                        'uses' => 'DestinasiController@update',
+                    ]
+                );
+                $router->delete(
+                    '/{id}',
+                    [
+                        'as' => 'destroy',
+                        'uses' => 'DestinasiController@destroy',
+                    ]
+                );
+            }
+        );
     }
 );
