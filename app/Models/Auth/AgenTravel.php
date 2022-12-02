f9875c0d2d5fa5c16a6ec0b8b270dfba94af7f5c
hisyam <hisyamyahya30@gmail.com>
2022-12-02 12:45:23 +0700

    model Agentravel

---
+++ app/Models/Auth/AgenTravel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destinasi extends Model {
+    
+    protected $table = 'agentravel';
+    public $description = 'Objek Wisata';
+
+    public const PERMISSIONS = [
+        'create'        => 'agentravel create',
+        'read'          => 'agentravel read',
+        'update'        => 'agentravel update',
+        'delete'        => 'agentravel delete',
+    ];
+
+    protected $fillable = [
+        'destinasi_id',
+        'alamt', 
+        'nama',
+        'contact_person',
+        'rating'
+    ];   
+}
