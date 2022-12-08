5e72e4b14593a7c32af530926c6bd3f4ef71c2f8
adamfarizi <adamfarizi4@gmail.com>
2022-12-08 11:51:26 +0700

    update image models

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+//use App\Models\Employee;
 
-class Image extends Model
-{
-    //
+class Image extends Model {
+    
+    protected $table = 'image';
+    public $desciption = 'Gambar';
+
+    public const PERMISSIONS = [
+        'create'        => 'image create',
+        'read'          => 'image read',
+        'update'        => 'image update',
+        'delete'        => 'image delete',
+    ];
+
+
+    //public function employee()
+    //{
+    //    return $this->belongsTo(Employee::class, 'nip');
+    //}
 }
