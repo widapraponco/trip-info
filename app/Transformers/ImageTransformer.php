dbffa55188515ca94e8ad3fb9014e50788e02bb9
hasbiyup <hasbi.reja005@gmail.com>
2022-12-11 22:11:03 +0700

    update TransformerImage

--- app/Transformers/ImageTransformer.php
+++ app/Transformers/ImageTransformer.php
@@ -1,38 +1,75 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: Lloric Mayuga Garcia <lloricode@gmail.com>
+ * Date: 11/24/18
+ * Time: 3:31 PM
+ */
+
 namespace App\Transformers;
 
-use League\Fractal\TransformerAbstract;
+use App\Models\Image;
+use App\Transformers\BaseTransformer;
 
-class ImageTransformer extends TransformerAbstract
+/**
+ * @OA\Schema(
+ *     schema="ImageTransformer",
+ *     type="object",
+ *     properties={
+ *         @OA\Property(property="id", type="string"),
+ *         @OA\Property(property="attributes", type="object", properties={
+ *
+ *             @OA\Property(property="name", type="string"),
+ *             @OA\Property(property="pic", type="string"),
+ *         }),
+ *         @OA\Property(property="relationships", type="array", @OA\Items({
+ *
+ *         })),
+ *         @OA\Property(property="meta", type="array", @OA\Items({
+ *
+ *             @OA\Property(property="include", type="array", @OA\Items({
+ *             })),
+ *         })),
+ *     }
+ * )
+ */
+class ImageTransformer extends BaseTransformer
 {
-    /**
-     * List of resources to automatically include
-     *
-     * @var array
-     */
-    protected array $defaultIncludes = [
-        //
-    ];
-    
-    /**
-     * List of resources possible to include
-     *
-     * @var array
-     */
     protected array $availableIncludes = [
-        //
+        'roles',
+        'permissions',
     ];
-    
+
     /**
      * A Fractal transformer.
      *
+     * @param  \App\Models\Image  $image
+     *
      * @return array
      */
-    public function transform()
+    public function transform(Image $image)
     {
-        return [
-            //
+        $response = [
+            'id'        => self::forId($image),
+            'name'      => $image->name,
+            'pic'    => $image->pic,
         ];
+
+        $response = $this->filterData(
+            $response,
+            [
+
+            ]
+        );
+
+        return $this->addTimesHumanReadable($image, $response);
+    }
+
+    /** @return string */
+    public function getResourceKey(): string
+    {
+        return 'image';
     }
 }
