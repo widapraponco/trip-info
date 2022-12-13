55b6e8c929967b0c6b97594b81c59f2135784c35
aprillia <maprillia650@gmail.com>
2022-12-13 14:02:26 +0700

    model souvenir

---
+++ app/Models/souvenir.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserCourses extends Model {
+    
+    protected $table = 'souvenir';
+    public $description = 'Kursus';
+
+    public const PERMISSIONS = [
+        'create'        => 'souvenir create',
+        'read'          => 'souvenir read',
+        'update'        => 'souvenir update',
+        'delete'        => 'souvenir delete',
+    ];
+
+    protected $fillable = [
+        'nip', 
+        'type',
+        'category',
+        'organized_by',
+        'year',
+        'certificate',
+        'other_info',
+    ];
+}
