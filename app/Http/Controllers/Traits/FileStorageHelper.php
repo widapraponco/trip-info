5f7634f4f4d49db2ecf4a5cd36c914d82b1c9d3f
hasbiyup <hasbi.reja005@gmail.com>
2022-12-13 15:45:33 +0700

    edit Traits

--- app/Http/Controllers/Traits/FileStorageHelper.php
+++ app/Http/Controllers/Traits/FileStorageHelper.php
@@ -4,14 +4,44 @@ namespace App\Http\Controllers\Traits;
 
 use Illuminate\Filesystem\FilesystemException;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\httpFoundation\File\UploadedFile;
+
+use App\Models\Image;
 
 trait FileStorageHelper {
 
-    public function reconstructionFile(array &$attributes = [], string $path)
+    public function images(array $files, string $destination)
     {
-        $attributes['name'] = $attributes['file']->getClientOriginalName();
-        $attributes['type'] = $attributes['file']->getClientMimeType();
-        $attributes['size'] = $attributes['file']->getSize();
-        $attributes['path'] = $path;
-    }
+        $images = [];
+        foreach($files as $key => $value){
+            if (empty($value)) continue;
+
+            $images[] = $this->images($value, $destination, $key);
+        }
+        return $images;
+    }
+    public function imagefile(UploadedFile $uploadFile, string $destination, string $name =null)
+    {
+        $path = $uploadFile->store($destination);
+
+        $image = new Image([
+            'name' => $name ?: $uploadfile->hashName(),
+            'originalName' => $uploadfile->getClientoriginalName(),
+            'originalExtension' => $uploadfile->getClientoriginalExtension(),
+            'mimeType' => $uploadfile->getClientmimeType(),
+            'size' => $uploadfile->getSize(),
+            'path' => $path()
+        ]);
+
+        $image->save();
+        return $image;
+    }
+
+    public function deleteallfile($model)
+    {
+        foreach($model->images as $image){
+            Storage::delete($image->path);
+            $image->delete();
+        }
+    }
 }
