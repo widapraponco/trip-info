f27faafa72aa25199c8c863541e3fa0cb5b47b1e
abi <abi.rahmawan01@students.uns.ac.id>
2022-12-13 15:57:51 +0700

    Update Create Destinasi Action

--- domain/Destinasi/Actions/CreateDestinasiAction.php
+++ domain/Destinasi/Actions/CreateDestinasiAction.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Domain\Destinasi\Actions;
 
-use App\Models\Auth\Destinasi\Destinasi;
+use App\Models\Destinasi;
 
 class CreateDestinasiAction
 {
     public function execute(array $attributes): Destinasi
     {
-        $attributes['password'] = app('hash')->make($attributes['password']);
-
         return Destinasi::create($attributes);
     }
 }
