81fbf61c16d5120e6c87416b4968bff8c4ff4005
Meisy <meisyanjarfika@student.uns.ac.id>
2022-12-14 09:06:38 +0700

    add migration

---
+++ database/migrations/2022_12_14_015026_create_table_sarana_prasarana.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_sarana_prasarana', function (Blueprint $table) {
+            $table->id();
+
+            $table-> integer(destinasi_id);
+
+            $table-> string (nama);
+
+            $table-> string (fungsional & nonfungsional);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_sarana_prasarana');
+    }
+};
