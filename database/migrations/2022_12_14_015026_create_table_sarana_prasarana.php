1c2174b7d787c9ba09d54898693f8d2fa13d6db8
Meisy <meisyanjarfika@student.uns.ac.id>
2022-12-14 22:16:24 +0700

    migration

--- database/migrations/2022_12_14_015026_create_table_sarana_prasarana.php
+++ database/migrations/2022_12_14_015026_create_table_sarana_prasarana.php
@@ -25,6 +25,8 @@ return new class extends Migration
             $table-> string (deskripsi);
 
             $table-> string (contact_person);
+
+            $table-> int (rating);
         });
     }
 
