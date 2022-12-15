8d5400395fd22ebae5f45235e02d2ecca39b06da
Meisy <meisyanjarfika@student.uns.ac.id>
2022-12-15 14:05:31 +0700

    add routes

---
+++ routes/sarana_prasana.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/** @var Laravel\Lumen\Routing\Router $router */
+
+$router->group(
+    [
+        'as' => 'sarana_prasarana',
+    ],
+    function () use ($router) {
+        $router->post('/sarana_prasarana', ['uses'=> 'sarana_prasaranaController@store',]);
+        
+    }
+);
