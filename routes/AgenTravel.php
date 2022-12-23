48dab52c8dcf8fd0c101d52ee79c8fa2c8b01302
yahya <hisyamyahya30@gmail.com>
2022-12-23 12:21:44 +0700

    routes

--- routes/AgenTravel.php
+++ routes/AgenTravel.php
@@ -6,7 +6,7 @@ $router->group(
     ],
     function () use ($router) {
         $router->post('/agentravel', ['uses'=> 'AgenTravelController@create',]);
-        $router->get('/agentravel', ['uses' => 'AgenTravelController@find',]);
+        $router->get('/agentravel', ['uses' => 'AgenTravelController@index',]);
         $router->get('/agentravel/{id}', ['uses' => 'AgenTravelController@findById',]);
         $router->put('/agentravel/{id}', ['uses'=> 'AgenTravelController@update',]);
         $router->delete('/agentravel/{id}', ['uses'=> 'AgenTravelController@destroy',]);
