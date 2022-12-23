1b2c93aa4f5633c40b40518ffb132303958f6131
yahya <hisyamyahya30@gmail.com>
2022-12-23 12:18:58 +0700

    domain

---
+++ domain/User/Actions/CreateAgenTravelAction.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\AgenTravel\Actions;
+
+use App\Models\AgenTravel;
+
+class FindAgenTravelByRouteKeyAction
+{
+    public function execute(string $routeKey, bool $throw404 = false): AgenTravel
+    {
+        $query =  AgenTravel::where((new AgenTravel())->getRouteKeyName(), $routeKey);
+
+        if ($throw404) {
+            return $query->firstOrFail();
+        }
+
+        return $query->first();
+    }
+}
